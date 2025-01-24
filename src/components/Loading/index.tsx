b0846b2a70333b4e80076eb734f6bf89643dd77d
duccini <guilhermeduccini@gmail.com>
2025-01-24 15:52:37 -0300

    create component tests with react testing library

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,15 @@
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, View } from "react-native";
 
-import { styles } from './styles';
-import { theme } from '@styles/theme';
+import { styles } from "./styles";
+import { theme } from "@styles/theme";
 
 export function Loading() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator color={theme.colors.blue_light} />
+      <ActivityIndicator
+        testID="activity-indicator"
+        color={theme.colors.blue_light}
+      />
     </View>
   );
 }
